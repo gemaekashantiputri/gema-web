fd2b9cb64fdb71d0c8b40807708d81d2bdc31fae
gemaekashantiputri <gemaeka1@gmail.com>
2022-02-18 11:44:31 +0700

    create landing part and Intro css

---
+++ src/components/landing.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/Intro.css';
+
+const Landing = () => {
+
+  return (
+    <div id="intro">
+      <div className="intro-subtitle">Hi, it’s Gema Eka Shanti Putri</div>
+      <div className="intro-desc">
+        I’m Front end developer and designer from Purwokerto.
+        I have interest in Web developer, Design, Illustration, Machine Learning.
+      </div>
+    </div>
+  );
+}
+
+export default Landing
