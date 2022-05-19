b9cc9423dff96e975702ef0117d22647bb6e7e47
gemaekashantiputri <gemaeka1@gmail.com>
2022-05-20 00:58:03 +0700

    repair index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './src/styles/intro.css';
-import './src/styles/sidebar.css';
 import App from './App';
 
 ReactDOM.render(
