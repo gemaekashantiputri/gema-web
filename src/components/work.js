7ef904431461862b15ecfde636c2838c364adbfe
gemaekashantiputri <gemaeka1@gmail.com>
2023-09-02 00:16:10 +0700

    add portfolio

--- src/components/work.js
+++ src/components/work.js
@@ -11,7 +11,7 @@ const Work = () => {
         <div className="work-subtitle">#Work</div>
         <div className="project-container">
           <ul className='projects-grid'>
-             <li className='projects-card'>
+            <li className='projects-card'>
               <div className='card-header'>
                 <div className='folder-icon'>
                   <FolderOpenRoundedIcon
@@ -20,13 +20,47 @@ const Work = () => {
                 </div>
               </div>
               <div className='work-img'>
-                <img src='/assets/overview.jpg' alt='overview'/>
+                <img src='/assets/box.jpg' alt='box'/>
               </div>
               <div className='card-title'>
-              <a href="https://www.behance.net/gallery/164829313/Redesign-Attendance-UMP">
-              Re-Design University Muhammadiyah Purwokerto Attendance</a></div>
-              <div className='card-desc'>An application for student attendance in each subject at University Muhammadiyah Purwokerto</div>
-              <div className='card-tech'>Google Form, Figma</div>
+              <a href="https://www.behance.net/gallery/179013971/Box-App-Ordering-Album">
+              Box App Ordering Album</a></div>
+              <div className='card-desc'>An application for people who wants buy album to support Idol</div>
+              <div className='card-tech'>Figma</div>
+            </li>
+            <li className='projects-card'>
+              <div className='card-header'>
+                <div className='folder-icon'>
+                  <FolderOpenRoundedIcon
+                    style={{ fontSize: 35 }}
+                  ></FolderOpenRoundedIcon>
+                </div>
+              </div>
+              <div className='work-img'>
+                <img src='/assets/bootcamp.png' alt='bootcamp'/>
+              </div>
+              <div className='card-title'>
+              <a href="https://www.behance.net/gallery/179026127/Bootcamp-App-Guide-Career?">
+              Bootcamp App Guided Career</a></div>
+              <div className='card-desc'>An application for people who wants learn new skill</div>
+              <div className='card-tech'>Figma</div>
+            </li>
+            <li className='projects-card'>
+              <div className='card-header'>
+                <div className='folder-icon'>
+                  <FolderOpenRoundedIcon
+                    style={{ fontSize: 35 }}
+                  ></FolderOpenRoundedIcon>
+                </div>
+              </div>
+              <div className='work-img'>
+                <img src='/assets/gems.png' alt='gems vet'/>
+              </div>
+              <div className='card-title'>
+              <a href="https://www.behance.net/gallery/179028611/Gems-Vet-Register-Checkup?">
+              Gem's Vet for pet checkup</a></div>
+              <div className='card-desc'>An application for people who wants do pet checkup</div>
+              <div className='card-tech'>Figma</div>
             </li>
           </ul>
         </div>
