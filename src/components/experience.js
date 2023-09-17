e344b2d29ebaef1e28e0994be6d8cf25cf70e546
gemaekashantiputri <gemaeka1@gmail.com>
2023-09-17 13:54:51 +0700

    spacing

--- src/components/experience.js
+++ src/components/experience.js
@@ -14,7 +14,7 @@ const Experience = () => {
             </div>
             <div className='joblist-duration'>June 2023 - September 2023</div>
             <div className='job-description'>
-              <li>-	Worked on projects within Grow with Google, especially user research and design interaction</li>
+              <li>Worked on projects within Grow with Google, especially user research and design interaction</li>
             </div>
           </li>
           <li className='job2'>
@@ -23,7 +23,7 @@ const Experience = () => {
             </div>
             <div className='joblist-duration'>August 2021 - January 2022</div>
             <div className='job-description'>
-              <li>-	Worked on a capstone project within the Dicoding platform by creating a functional website with the theme Resource Sharing & User Generated Content</li>
+              <li>Worked on a capstone project within the Dicoding platform by creating a functional website with the theme Resource Sharing & User Generated Content</li>
             </div>
           </li>
           <li className='job3'>
@@ -32,7 +32,7 @@ const Experience = () => {
             </div>
             <div className='joblist-duration'>July 2020 - September 2021</div>
             <div className='job-description'>
-              <li>-	Conducted lectures under Mr. Abdul Haris Mulyadi by assessing student performance</li>
+              <li>Conducted lectures under Mr. Abdul Haris Mulyadi by assessing student performance</li>
             </div>
           </li>
         </ul>
